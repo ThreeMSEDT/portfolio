5212aec4c584042b58c06df370cf9970b698d7c2
Mustafa <218740897+0x99M@users.noreply.github.com>
2025-07-02 11:48:19 +0300

    Update home page header and subheader

--- src/app/fonts.ts
+++
@@ -1,6 +0,0 @@
-import { Megrim } from 'next/font/google'
-
-export const megrim = Megrim({
-  subsets: ['latin'],
-  weight: '400',
-})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,45 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { megrim } from './fonts';
+import Link from 'next/link';
 
-const letters = ['3', 'M', 'S', 'E', 'D', 'T']
+const header = 'MUSTAFA'.split('');
+const subheader = 'Muslim  Software Engineer  Day Trader'.split('  ');
+const routes = ['/muslim', '/software-engineer', '/day-trader'];
 
 export default function Home() {
   return (
-    <div className={`w-full h-full flex items-center justify-center gap-24 text-9xl ${megrim.className}`} >
-      {letters.map((char, i) => (
-        <motion.div
-          key={i}
-          initial={{ opacity: 0, y: 40 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: i * 0.2, duration: 0.5, ease: 'easeOut' }}
-        >
-          {char}
-        </motion.div>
-      ))}
+    <div className='w-full h-full flex flex-col gap-8 items-center justify-center' >
+      <div className='flex gap-4 items-center justify-center text-9xl'>
+        {header.map((char, i) => (
+          <motion.div
+            key={i}
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: i * 0.2, duration: 0.5, ease: 'easeOut' }}
+          >
+            {char}
+          </motion.div>
+        ))}
+      </div>
+      <div className='flex gap-4 items-center justify-center text-xl text-center text-[#AEAEAE]'>
+        {subheader.map((part, i) => (
+          <Link href={routes[i]} key={i}>
+            <motion.div
+              className="cursor-pointer hover:text-[#E3E3E3]"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                delay: header.length * 0.2 + i * 0.2,
+                duration: 0.5,
+                ease: 'easeOut',
+              }}
+            >
+              {part}
+            </motion.div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
