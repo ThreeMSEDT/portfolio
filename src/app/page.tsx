c9c1780cf7657c117b0c16bc81277ad6a80f1e63
Mustafa <218740897+0x99M@users.noreply.github.com>
2025-07-07 09:16:57 +0300

    Change home page style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,51 +3,36 @@
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 
-const header = 'MUSTAFA'.split('');
-const subheader = 'Muslim  Software Engineer  Day Trader'.split('  ');
+const options = 'Muslim  Software Engineer  Day Trader'.split('  ');
 const routes = ['/muslim', '/software-engineer', '/day-trader'];
 
 export default function Home() {
   return (
-    <div className='w-full h-full flex flex-col gap-4 md:gap-8 items-center justify-center' >
-      <div className='flex flex-wrap gap-2 sm:gap-4 items-center justify-center text-6xl sm:text-7xl md:text-8xl lg:text-9xl'>
-        {header.map((char, i) => (
+    <div className='w-full h-full flex flex-col flex-wrap gap-4 sm:gap-6 items-center justify-center text-xl sm:text-2xl md:text-3xl text-center text-[#6b6b6b]'>
+      {options.map((option, i) => (
+        <Link href={routes[i]} key={i}>
           <motion.div
-            key={i}
-            initial={{ opacity: 0, y: 40 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: i * 0.2, duration: 0.5, ease: 'easeOut' }}
-          >
-            {char}
-          </motion.div>
-        ))}
-      </div>
-      <div className='flex flex-wrap gap-2 sm:gap-4 items-center justify-center text-base sm:text-lg md:text-xl text-center text-[#AEAEAE]'>
-        {subheader.map((part, i) => (
-          <Link href={routes[i]} key={i}>
-            <motion.div
-              className={`relative inline-block cursor-pointer
-              hover:text-[#E3E3E3]
+            className={`relative inline-block cursor-pointer
+              hover:text-[#252525]
               before:content-[''] before:absolute before:bottom-0 before:left-0
-              before:h-[2px] before:w-full before:bg-[#E3E3E3]
+              before:h-[2px] before:w-full before:bg-[#252525]
               before:rounded-full
               before:origin-center
               before:scale-x-0
               before:transition-transform before:duration-300
               hover:before:scale-x-100`}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{
-                delay: header.length * 0.2 + i * 0.2,
-                duration: 0.5,
-                ease: 'easeOut',
-              }}
-            >
-              {part}
-            </motion.div>
-          </Link>
-        ))}
-      </div>
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: i * 0.2,
+              duration: 0.5,
+              ease: 'easeOut',
+            }}
+          >
+            {option}
+          </motion.div>
+        </Link>
+      ))}
     </div>
   );
 }
